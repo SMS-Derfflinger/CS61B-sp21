acc696c1a76755c4043251b3606f9797e2726b35
Heinz <aurelianob808@gmail.com>
2024-07-21 13:34:14 +0800

    finished lab3 Timing Tests for List61B

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -23,5 +23,26 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<Integer>();
+        Ns.addLast(1000);
+        Ns.addLast(2000);
+        Ns.addLast(4000);
+        Ns.addLast(8000);
+        Ns.addLast(16000);
+        Ns.addLast(32000);
+        Ns.addLast(64000);
+        Ns.addLast(128000);
+
+        AList<Double> times = new AList<Double>();
+        for (int i = 0; i < Ns.size(); i++) {
+            Stopwatch sw = new Stopwatch();
+            AList<Integer> temp = new AList<Integer>();
+            for (int j = 0; j < Ns.get(i); j++) {
+                temp.addLast(j);
+            }
+            double timeInSeconds = sw.elapsedTime();
+            times.addLast(timeInSeconds);
+        }
+        printTimingTable(Ns, times, Ns);
     }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,5 +1,6 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import net.sf.saxon.expr.Component;
 
 /**
  * Created by hug.
@@ -23,6 +24,34 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<Integer>();
+        Ns.addLast(1000);
+        Ns.addLast(2000);
+        Ns.addLast(4000);
+        Ns.addLast(8000);
+        Ns.addLast(16000);
+        Ns.addLast(32000);
+        Ns.addLast(64000);
+        Ns.addLast(128000);
+
+        int M = 10000;
+        AList<Integer> ops = new AList<Integer>();
+        AList<Double> times = new AList<Double>();
+        for (int i = 0; i < Ns.size(); i++) {
+            SLList<Integer> temp = new SLList<Integer>();
+            for (int j = 0; j < Ns.get(i); j++) {
+                temp.addLast(j);
+            }
+
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < M; j++) {
+                temp.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+            ops.addLast(M);
+            times.addLast(timeInSeconds);
+        }
+        printTimingTable(Ns, times, ops);
     }
 
 }
